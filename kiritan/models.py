d1f8b275e68bb7b96d69f0bd783bce4792d1a54a
kohishibashi <trendy8455@gmail.com>
2020-10-10 03:58:26 +0900

    03 モデル追加

--- kiritan/models.py
+++ kiritan/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField('タイトル', max_length=200)
+    complete = models.BooleanField('完了', default=False)
+    updated_at = models.DateTimeField('更新', auto_now=True)
+    creted_at = models.DateTimeField('作成日', auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- kiritan/views.py
+++ kiritan/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 
 def home(request):
-    return render(request, 'kiritan/top.html')
+    tasks = Task.objects.all()
+
+    context = {
+        'tasks': tasks,
+    }
+    return render(request, 'kiritan/top.html', context)
 
 
 def sub(request):
