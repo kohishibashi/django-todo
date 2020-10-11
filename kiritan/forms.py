77471a020c6fc180557135c459c0e567b0434ca7
kohishibashi <trendy8455@gmail.com>
2020-10-12 00:07:03 +0900

    05 見た目調整

--- kiritan/forms.py
+++ kiritan/forms.py
@@ -5,6 +5,10 @@ from .models import *
 
 
 class TaskForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(TaskForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
 
     class Meta:
         model = Task
